a2b6b22d8a46b96bf0d5f280e07cd40c1bdfa840
Nikita_Beliy <beliynikita1995@gmail.com>
2023-02-05 14:29:52 +0200

    refresh task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,44 +1,117 @@
 // Add imports above this line
+// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
+
+// Додай бібліотеку SimpleLightbox як залежність проекту, використовуючи npm (посилання на CDN з твоєї минулої роботи більше не потрібне).
+// Використовуй свій JavaScript код з попередньої домашньої роботи, але виконай рефакторинг з урахуванням того, що бібліотека була встановлена через npm (синтаксис import/export).
+// Для того щоб підключити CSS код бібліотеки в проект, необхідно додати ще один імпорт, крім того, що описаний в документації.
+
+// // Описаний в документації
+// import SimpleLightbox from "simplelightbox";
+// // Додатковий імпорт стилів
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-
-console.log(galleryItems);
+// Add imports above this line
 // Change code below this line
-const galleryRef = document.querySelector('.gallery');
-const galleryMarkup = createGalleryItemCard(galleryItems);
-galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
 
-galleryRef.addEventListener("click", showLargeImg)
 
-function createGalleryItemCard(galleryItem) {
-    return galleryItems.map(({preview, original, description}) => 
-    `
-    <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", getLargeImage);
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+const markup = galleryItems.forEach(({preview, original, description}) => {
+    const markup = `
+    <a 
+        class="gallery__item" 
+        href=${original}>
+            <img 
+                class="gallery__image" 
+                src=${preview} 
+                alt=${description}
             />
-        </a>
-    </div>
-    `).join("")
-};
+  </a>`
+    gallery.insertAdjacentHTML("beforeend", markup)
+})
 
-function showLargeImg(event) {
-    
-    if (!event.target.classList.contains("gallery__image")) {
-        return;
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+function getLargeImage(event) {
+    event.preventDefault();
+
+    if(event.target.nodeName === "IMG") {
+        var lightbox = new SimpleLightbox('.gallery a', 
+        {     
+            captionsData: "alt",
+            captionsDelay: "250ms"
+        });
     }
 
-    event.preventDefault()
-};
-const lightBox = new SimpleLightbox('.gallery a', {
-    captionsData: "alt",
-    captionsDelay: "250ms"
- })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { galleryItems } from './gallery-items';
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
+// console.log(galleryItems);
+// // Change code below this line
+// const galleryRef = document.querySelector('.gallery');
+// const galleryMarkup = createGalleryItemCard(galleryItems);
+// galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// galleryRef.addEventListener("click", showLargeImg)
+
+// function createGalleryItemCard(galleryItem) {
+//     return galleryItems.map(({preview, original, description}) => 
+//     `
+//     <div class="gallery__item">
+//         <a class="gallery__link" href="${original}">
+//             <img
+//             class="gallery__image"
+//             src="${preview}"
+//             data-source="${original}"
+//             alt="${description}"
+//             />
+//         </a>
+//     </div>
+//     `).join("")
+// };
+
+// function showLargeImg(event) {
+    
+//     if (!event.target.classList.contains("gallery__image")) {
+//         return;
+//     }
+
+//     event.preventDefault()
+// };
+// const lightBox = new SimpleLightbox('.gallery a', {
+//     captionsData: "alt",
+//     captionsDelay: "250ms"
+//  })
 
 
 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,25 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+import throttle from "lodash.throttle";
 
-const KEY = "videoplayer-current-time";
-const player = new Player('vimeo-player');
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', throttle((data) =>
-  localStorage.setItem(KEY, data.seconds), 1000)
-);
+player.on('timeupdate', throttle((event) => {
+    let currentTime = event.seconds;
+    console.log(currentTime)
+    localStorage.setItem("videoplayer-current-time", currentTime);
+}, 1000));
 
-const savedTime = localStorage.getItem(KEY);
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
 
-if (savedTime) {
-  player.setCurrentTime(savedTime);
-}
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
